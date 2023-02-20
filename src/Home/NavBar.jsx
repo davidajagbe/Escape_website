9cd447b6455b0e8f714c2aaddd7b454a69b89b78
Ajagbe David <davidajagbe2004@gmail.com>
2023-02-19 23:01:17 -0800

    updated images

--- src/Home/NavBar.jsx
+++ src/Home/NavBar.jsx
@@ -12,8 +12,8 @@ const NavBar = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" /> 
                 <Navbar.Collapse id="basic-navbar-nav"/>
                 <Nav className='navlist me-right ' >
-                    <Nav.Link href='#home' className='mx-5'>HOME</Nav.Link>
-                    <Nav.Link href='#categories' className='mx-5'>CATEGORIES</Nav.Link>
+                    <Nav.Link href='#home' className='mx-3'>HOME</Nav.Link>
+                    <Nav.Link href='#categories' className='mx-3'>CATEGORIES</Nav.Link>
                     <Nav.Link href='#about' className='mx-3'>ABOUT</Nav.Link>
                     <Nav.Link href='#contact' className='mx-3'>CONTACT</Nav.Link>
                 </Nav>
--- src/Home/Posts.jsx
+++ src/Home/Posts.jsx
@@ -1,18 +1,44 @@
 import React from 'react'
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import './Posts.css';
 
 const Posts = () => {
   return (
     <section className='main-section post-section'>
-        <Container>
-            <div className="section-heading">
-                <div className="section-title">
-                    <h2>Featured Posts</h2>
+        <Container className='featured-post'>
+          <div className="section-heading">
+              <div className="section-title">
+                  <h2>Featured Posts</h2>
+              </div>
+              <div className="divider-rule"></div>
+          </div>
+        </Container >
+        <Row className='latest-post latest-Row'>
+          <Col className='featured-thumbnail photography-col f-col'>
+            <a href="#photograpghy" className="featured-wrapper f-inline-block">
+              <div>
+                <div className="category-tag">Photograghy</div>
+                <div className='featured-image'></div>
+              </div>
+              <div className="featured-text">
+                <div className="featured-title">The Road Ahead</div>
+                <div class="featured-description"><p> The road ahead might be paved - it might not be.</p></div>
+                <div className="featured-details">
+                  <div className="clearfix">
+                    <img src="" alt="" className='author-img'/>
+                    <div className="author-title lite"></div>
+                    <div className="thumbnail date lite"></div>
+                  </div>
                 </div>
-                <div className="divider-rule"></div>
-            </div>
-        </Container>
+              </div>
+            </a>
+          </Col>
+          <Col className='featured-thumbnail adventure-col f-col'>
+            <a href="#adventure" className="featured-wrapper f-inline-block">
+
+            </a>
+          </Col>
+        </Row>
     </section>
   )
 }
