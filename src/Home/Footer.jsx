069683b48b54f12d6b9c692e86f7e89220566dcf
Ajagbe David <davidajagbe2004@gmail.com>
2023-06-24 21:45:34 +0100

    Updated escrow web

--- src/Home/Footer.jsx
+++ src/Home/Footer.jsx
@@ -3,6 +3,18 @@ import { Container } from 'react-bootstrap';
 import './Footer.css'
 
 const Footer = () => {
+  const message = false;
+
+  if (message === onclick.Submit){
+    <div className={message}>
+      <p>Thank you! Your submission has been received!</p>
+    </div>
+  }
+  else{
+    <div class="w-form-fail">
+      <p>Oops! Something went wrong while submitting the form</p>
+    </div>
+  }
   return (
     <section className='main-section dark f-section'>
         <Container className='f-container'>
@@ -13,14 +25,16 @@ const Footer = () => {
                 <div className="med-divider"></div>
             </div>
             <div className="form-wrapper w-form">
-                <form 
-                  name='email-form'
-                  method='get'
-                  placeholder='Enter Your Email Addresses'
-                  className="f-clearfix"
-                >
-                    <input type="text" className="field-input" />
-                </form>
+              <form 
+                name='email-form'
+                method='get'
+                className="f-clearfix"
+              >
+                <div class="input-group mt-3 border-0">
+                  <input type='text' className='form-control' placeHolder='Enter your Email Address' ariaLabel='Enter your Email Address' ariaDescribedby='button-addon2' required=""/>
+                  <button className='btn btn-secondary' type='submit' id='button-addon2' value={"Submit"} dataWait="Please wait..." onClick={"Submit"}>Submit</button>
+                </div>
+              </form>
             </div>
         </Container>
         <div className="footer-section">
